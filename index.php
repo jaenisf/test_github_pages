cba71712faab09392279dae5b659fdd0c306abeb
jaenisf <66373378+jaenisf@users.noreply.github.com>
2020-06-03 16:23:47 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Example</title>
+    </head>
+    <body>
+        <p>This is an example of a simple HTML page with one paragraph.</p>
+        <br>
+        <hr>
+        <br>
+        <?php echo "Hello World!"; ?>
+    </body>
+</html>
